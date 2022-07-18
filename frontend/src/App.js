348f38a324d10ef1fab8df674e4ecca485beebac
batatonana <fx31003@gmail.com>
2022-07-18 13:44:53 +0100

    fornt end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import "./index.css";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="pagee">
+        <div className="page">
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,32 +1,32 @@
 import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
+import "../index.css";
 const Home = () => {
   const [links, setLinks] = useState(null);
   const [date, setDate] = useState();
 
-  const handleSubmit = async(e) =>{
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const aux = {
-      month: (parseInt(JSON.stringify(date).split('-')[1])).toString(),
-      year: JSON.stringify(date).split('-')[0].split('"')[1],
-    }
-    const response = await fetch('http://localhost:4000', {
-      method: 'POST',
+      month: parseInt(JSON.stringify(date).split("-")[1]).toString(),
+      year: JSON.stringify(date).split("-")[0].split('"')[1],
+    };
+    const response = await fetch("http://localhost:4000", {
+      method: "POST",
       body: JSON.stringify(aux),
-      headers:{
-        'Content-Type': 'application/json'
-      }
-    })
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     const json = await response.json();
-    if(!response.ok){
-      console.log("ERROR")
+    if (!response.ok) {
+      console.log("ERROR");
     }
-    if(response.ok){
+    if (response.ok) {
       setLinks(json);
     }
-  }
+  };
 
   useEffect(() => {
     const fetchLinks = async () => {
@@ -40,9 +40,9 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    if (links != null  && Object.keys(links.links).length !== 0) {
+    if (links != null && Object.keys(links.links).length !== 0) {
       setDate(
-        new Date(Date.UTC(links.links[0].date[0], links.links[0].date[1]-1))
+        new Date(Date.UTC(links.links[0].date[0], links.links[0].date[1] - 1))
       );
     }
     // eslint-disable-next-line
@@ -50,33 +50,29 @@ const Home = () => {
 
   if (links != null) {
     return (
-      <div className="home">
-        <h1>
-          Download links from
-        </h1>
-        <br />
-        <form className="datemodify" onSubmit={handleSubmit}>
-          <label>Select Date: </label>
-          <DatePicker
-            selected={date}
-            onChange={(e) => setDate(e)}
-            dateFormat="MM/yyyy"
-            showMonthYearPicker
-            showFullMonthYearPicker
-          />
-          <button>Search</button>
-        </form>
-        <br />
-        <br />
+      <div>
+        <div className="home">
+          <h1>Download links from: </h1>
+          <form className="datemodify" onSubmit={handleSubmit}>
+            <DatePicker
+              wrapperClassName="date-picker"
+              selected={date}
+              onChange={(e) => setDate(e)}
+              dateFormat="MM/yyyy"
+              showMonthYearPicker
+              showFullMonthYearPicker
+            />
+            <button className="form-b">Search</button>
+          </form>
+        </div>
         <div className="links">
-          {Object.keys(links.links).length === 0 &&
-          <h2>No downloads available for this date</h2>
-          }
+          {Object.keys(links.links).length === 0 && (
+            <h2>No downloads available for this date</h2>
+          )}
           {links &&
             links.links.map((link) => (
               <a key={link.id} href={link.url}>
                 {link.url}
-                <br />
                 <br />
               </a>
             ))}
